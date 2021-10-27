ebe54ab43a7b4e198322a72e6d7e2dfc89c99ed6
Olenz1 <54223329+Olenz1@users.noreply.github.com>
2021-10-27 23:29:15 +0800

    Update for_you_to_do.c

--- src/for_you_to_do.c
+++ src/for_you_to_do.c
@@ -181,7 +181,83 @@ void mydgemm(double *A, double *B, double *C, int n, int i, int j, int k, int b)
  * 
  **/
 int mydgetrf_block(double *A, int *ipiv, int n, int b) 
-{
+{/*
+    int ib, t, j, k;
+    for (ib = 0; ib < n - 1; ib += b)
+    {
+        int end = ib + b - 1;
+        int maxind = ib;
+        int max = abs(A[ib * n + ib]);
+        for (t = ib; t < n; t += b)
+            if (abs(A[t * n + ib]) > max)
+            {
+                maxind = t;
+                max = abs(A[t * n + ib]);
+            }
+        if (max == 0)   return -1;
+        else {
+            if (maxind != ib)
+            {
+                //save pivoting infomation
+                int temps = ipiv[ib];
+                ipiv[ib] = ipiv[maxind];
+                ipiv[maxind] = temps;
+                //swap rows
+                int j;
+                for (j = 0; j < n; j += b)
+                {
+                    int tempv = A[n * ib + j];
+                    A[ib * n + j] = A[maxind * n + j];
+                    A[maxind * n + j] = tempv;
+                }
+            }
+        }
+        //factorization
+        // for (j = ib; j < n; j ++)
+        //     A[j * n + ib] /= A[ib * n + ib];
+        // for (j = ib; j < n; j ++)
+        //     for (k = ib; k < n; k ++)
+        //         A[j * n + k] = A[j * n + k] - A[j * n + ib] * A[ib * n + k]
+        
+        for (j = ib - 1; ib < end; ib += b)
+        {
+            for (k = end; k < n; k += b)
+                A[j * n + k] = LL-1 * A[j * n + k];
+            for (k = end; k < n; k += b)  
+                for (k1 = end; k1 < n; k1 += b)
+                    A[k * n + k1] -= A[k * n + j] * A[j * n + k];
+        }
+    }*/
     return 0;
 }
 
+// for i = 1 to n-1
+//      A(i+1:n,i) = A(i+1:n,i) / A(i,i)         … BLAS 1 (scale a vector)
+//      A(i+1:n,i+1:n) = A(i+1:n , i+1:n )  … BLAS 2 (rank-1 update)
+//               - A(i+1:n , i) * A(i , i+1:n)
+
+//     i = i
+//     j = i+1:n
+//     k = i+1:n
+
+//         for (j = i; j < n; j ++)
+//             A[j * n + i] /= A[i * n + i];
+//         for (j = i; j < n; j ++)
+//             for (k = i; k < n; k ++)
+//                 A[j * n + k] = A[j * n + k] - A[j * n + i] * A[i * n + k]
+
+// for ib = 1:n-1   b
+//      A(ib:end , end+1:n) = LL-1 * A(ib:end , end+1:n)
+//      A(end+1:n , end+1:n ) = A(end+1:n , end+1:n )
+//                   - A(end+1:n , ib:end) * A(ib:end , end+1:n) 
+
+//     i = ib
+//     j = ib:end
+//     k = end+1:n
+
+// for (j = ib - 1; ib < end; ib += b)
+//     for (k = end; k < n; k += b)
+//         A[j * n + k] = LL-1 * A[j * n + k];
+//     for (k = end; k < n; k += b)  
+//         for (k1 = end; k1 < n; k1 += b)
+//             A[k * n + k1] -= A[k * n + j] * A[j * n + k];
